1412cfadfdaeb7521d21a140f275480aea1a56b7
Daniil5343 <88218114+Daniil5343@users.noreply.github.com>
2025-05-27 15:14:14 -0400

    Stable: Gorilla_FSM
    The most recent and stable version of this FSM.
    
    Changes:
    -Rush State
    -States can exit cleanly now
    -Zoning behavior is as solid as can be for SS operations
    -Transitions Noted
    -Minor fixes
    
    TBA:
    -Attack Methods
    -Complete(?) Methods
    -Sweeping Optimizations

--- NPC_Items/Gorilla_FSM.lua
+++ NPC_Items/Gorilla_FSM.lua
@@ -20,6 +20,10 @@ local gorillaP1_Punch1 = repStorage.GorillaAnims.Attacks.gorillaPhase1_punch1
 local gorillaP1_Punch2 = repStorage.GorillaAnims.Attacks.gorillaPhase1_punch2
 local gorillaP1_Sweep =  repStorage.GorillaAnims.Attacks.gorillaPhase1_sweep
 
+local ATTACK_RANGE = 6
+local SEARCH_RANGE = 80
+local CHASE_RANGE = 15
+
 local control = {}
 control.__index = control
 
@@ -38,7 +42,7 @@ function control.spawnGorilla(Gorilla, pos: CFrame)
 	warn("Gorilla Spawn:", Gorilla)
 	Gorilla.Parent = game.Workspace
 	Gorilla.Humanoid.HipHeight = 3.45
-	Gorilla.Humanoid.WalkSpeed = 24
+	Gorilla.Humanoid.WalkSpeed = 20
 	Gorilla.PrimaryPart.CFrame = pos or CFrame.new(32, 56, -10)
 	warn("Hip Height:", Gorilla.Humanoid.HipHeight)
 	Gorilla.PrimaryPart:SetNetworkOwner(nil)
@@ -62,48 +66,47 @@ function blockedMovement(GorillaRoot, enemyModel)
 	return false
 end
 
-function locateEnemy(GorillaRoot: BasePart) --Model, Num
-	local nearestEnemy, dist = nil, 50
+function locateEnemy(GorillaRoot: BasePart): Model --Model, Num
+	local nearestEnemy, dist = nil, 80
 	local distTable = {}
 	
-	for _, chara: Model in workspace.PlayerFolder:GetChildren() do 
-		local root = chara.PrimaryPart
-		 local rootDist = (root.Position - GorillaRoot.Position).Magnitude
-		 if rootDist <= 50 then	
-			--warn("Player Within visible distance.")
-			distTable[root.Parent] = rootDist
-		end
-	end
+	if workspace.PlayerFolder:GetChildren() then
+			for _, chara: Model in workspace.PlayerFolder:GetChildren() do 
+				local root = chara.PrimaryPart
+				 local rootDist = (root.Position - GorillaRoot.Position).Magnitude
+				 if rootDist <= SEARCH_RANGE then	
+					--warn("Player Within visible distance.")
+					distTable[root.Parent] = rootDist
+				end
+			end
 	for enemy, mag in distTable do 
 		if mag <= dist then 
 			dist = mag
-			--print("ENEMY:", enemy)
+			--print("NEAREST ENEMY:", enemy)
 			nearestEnemy = enemy
 		end
 	end
 		return nearestEnemy, dist
+	end
+	return nil, nil
 end
 
-function distanceCheck(GorillaRoot: BasePart,enemyRoot: BasePart)
+function distanceCheck(GorillaRoot: BasePart,enemyRoot: BasePart): number
 		return (enemyRoot.Position - GorillaRoot.Position).Magnitude
 end
 
-function directMove(GorillaRoot: BasePart, position: Vector3)
-	GorillaRoot.Parent.Humanoid:MoveTo(position)
-end
-
-function moveTowards(GorillaRoot,enemyModel)
-		warn("ENEMY MODEL?:", enemyModel)
+function moveTowards(GorillaRoot,enemyModel): string
+	warn("ENEMY MODEL?:", enemyModel)
+		if enemyModel == nil then 
+			return "Idle"
+		end
+	
 		local enemyRoot = enemyModel.HumanoidRootPart
 		local gorillaHum: Humanoid = GorillaRoot.Parent.Humanoid
 		local gorillaAnim: Animator = gorillaHum.Animator
+		gorillaHum.WalkSpeed = 32
 		local runAnimation: AnimationTrack = GorillaRoot.Parent.Humanoid.Animator:LoadAnimation(repStorage.GorillaAnims.Actions.gorillaPhase1_run)
 		runAnimation.Priority = Enum.AnimationPriority.Movement
-		
-		local ATTACK_DISTANCE = 9
-		local TIMEOUT_POINT = 3
-		local RECALCULATE_DIST = 15
-		local pathTargetPos = enemyRoot.Position
 		
 		local chargePath = pathFindingSRVC:CreatePath({AgentHeight = 8})
 		local success, err = pcall(function()
@@ -112,71 +115,54 @@ function moveTowards(GorillaRoot,enemyModel)
 		
 		local waypoints 
 		
-		if success and chargePath.Status == Enum.PathStatus.Success then
+		if success then 
 			waypoints = chargePath:GetWaypoints()
 			local animTracks = gorillaAnim:GetPlayingAnimationTracks() 
-				for i, v in animTracks do 
-					if v.Name == "gorillaPhase1_run" then
-						v:Stop()
-						end
-					end
-		warn("Path Created with:", waypoints, "waypoints.")
-		else
-			warn("--PATH FAILURE, ATTEMPTIND DIRECT MOVE")
-			directMove(GorillaRoot, pathTargetPos)
-			task.wait(.5)
-			return "Charging"
+			
+		if #waypoints <= 2 and math.abs((waypoints[1].Position.Y - GorillaRoot.Position.Y)) < 5  then
+			warn("WAYPOINTS TOO SMALL, RUSHING")
+			return "Rush"
 		end
-		
-		if #waypoints < 3 then 
-			warn("Calculated path too short. Directly moving.")
-			directMove(GorillaRoot, pathTargetPos)
-			task.wait(.5)
-			return "Charging"
-		end
-		
+	
+			for i, v in animTracks do 
+				if v.Name == "gorillaPhase1_run" then
+					v:Stop()
+				end
+			end
+			
 		runAnimation:Play()
 		
-		for i = 1, #waypoints do --1 is always starting.
-			warn("WayPoint:", i)
-			warn("Moving")
-			local point = waypoints[i]
+		print("NUMBER OF POINTS:", #waypoints)
+		for i, point in  waypoints do
+				gorillaHum:MoveTo(point.Position)
+				gorillaHum.MoveToFinished:Wait()
 			
-			local timeWaited = 0
-			
-			while (GorillaRoot.Position - point.Position).Magnitude > 7 and timeWaited < TIMEOUT_POINT do 
-				local currentEnemy, currentDist = locateEnemy(GorillaRoot)
-				
-				if not currentEnemy then
-					runAnimation:Stop()
-					return "Idle"
-				end
-				
-				if currentDist <= ATTACK_DISTANCE then
-					gorillaHum:MoveTo(GorillaRoot.Position)
-					runAnimation:Stop()
-					return "Attacking"
-				end
-				
-				if (currentEnemy.HumanoidRootPart.Position - pathTargetPos).Magnitude > RECALCULATE_DIST then
-					warn("Target strayed too far from path. new path needed.")
-					runAnimation:Stop()
-					return "Charging"
-				end
-				
-			task.wait(.1) -- so the loop doesn't explode on us
-			TIMEOUT_POINT += .1
-			
-			if timeWaited > TIMEOUT_POINT then
-					warn("Path took too long or got stuck, recalculating.")
-					runAnimation:Stop()
-					return "Charging"
+			if (enemyRoot.Position - waypoints[#waypoints].Position).Magnitude > 20 then 
+				warn("TARGET HAS STRAYED TOO FAR RE-CALCING.")
+				runAnimation:Stop()
+				return "Charging"
 			end
 			
-			end
+				if distanceCheck(GorillaRoot, enemyRoot) < 10 then
+					runAnimation:Stop()
+					warn("CLOSE ENOUGH, RETURNING ATTACK/RUSH FROM CHARGE")
+					return "Rush"
+				end
 		end
-		
-end--Pathfinding or Unit MoveTo | BOTH? | yes b/c If player moves too far it becomes naught.
+		runAnimation:Stop()
+		warn("Path Completed with no returns")
+		return "Charging"
+		else
+			warn("PATH FAILED TO CALCULATE FROM", GorillaRoot.Parent, "to", enemyRoot.Parent)
+			return "Idle"
+		end--Pathfinding or Unit MoveTo | BOTH? | yes b/c If player moves too far it becomes naught.
+		 --return true
+end
+
+function directMove(GorillaRoot: BasePart, position: Vector3)
+	GorillaRoot.Parent.Humanoid:MoveTo(position)
+	
+end
 
 function idleMovement(GorillaRoot: BasePart)--Load animations in state?
 	local randX = math.random(GorillaRoot.Position.X-15,GorillaRoot.Position.X+15)
@@ -209,8 +195,7 @@ type stateType = {
 }
 
 local function attackMethod(GorillaRoot, enemyModel)--Now has to account for ranged Attacks
-		warn("Gorilla is attacking:", enemyModel)
-		return true 
+	warn("Gorilla is attacking:", enemyModel)
 end
 
 ---| STATE BEHAVIOR
@@ -219,32 +204,83 @@ function idleState_Action(GorillaRoot: BasePart)
 	warn("Idle Began")
 	local moved = idleMovement(GorillaRoot)
 	local enemy, dist = locateEnemy(GorillaRoot)
-	if enemy and dist then -- Chasing/Ranged range
+	if enemy and dist < 50 then -- Chasing/Ranged range
 		return "Charging" 
 	end
-end
-
-function chargingState_Action(GorillaRoot: BasePart)
-	local enemy = locateEnemy(GorillaRoot)
-	local moveResult = moveTowards(GorillaRoot, enemy)
-	
-	if moveResult == "Attacking" then
-		return "Attacking"
-	end
-
 	return "Idle"
 end
 
+function chargingState_Action(GorillaRoot: BasePart)
+	local enemy, dist = locateEnemy(GorillaRoot)
+	local moveResult
+	
+	if dist < 50 then 
+		moveResult = moveTowards(GorillaRoot, enemy)
+	end
+	
+	if moveResult == "Idle" then
+		warn("CHARGING RETURNING: IDLE")
+		return "Idle"
+	end
+	
+	if moveResult == "Rush" then
+		warn("CHARGING RETURNING: RUSH")
+		return "Rush"
+	end
+	
+
+	if locateEnemy(GorillaRoot) then 
+		return "Charging"
+	else
+		return "Idle"
+	end
+end
+
 function attackState_Action(GorillaRoot: BasePart)
-		local enemy = locateEnemy(GorillaRoot)
+		local enemy, dist = locateEnemy(GorillaRoot)
 		local attackResult = attackMethod(GorillaRoot, enemy)
-		local enemyCheck, dist = locateEnemy(GorillaRoot)
-		
-		if enemyCheck and dist<10 then 
+		warn("Attack called", enemy)	
+	
+		if enemy and dist <= 5 then
 			return "Attacking"
-		else
-			return "Idle"
 		end
+	
+	_, dist = locateEnemy(GorillaRoot) 
+	
+	if enemy and dist > 3 or dist < 15 then
+		return "Rush"
+	else if enemy and dist > 20 and dist < 50 then
+			return "Charging"
+		end
+	end		
+ return "Idle"
+end
+
+function rushState_Action(GorillaRoot: BasePart)
+	local enemyModel, dist = locateEnemy(GorillaRoot)
+	local GorillaHum = GorillaRoot.Parent.Humanoid :: Humanoid
+	GorillaHum.WalkSpeed = 32
+	
+	if dist <= 5 then
+		return "Attacking"
+	end
+	
+	while dist < 25 and enemyModel and enemyModel.Humanoid.Health > 0 do 
+		enemyModel, dist = locateEnemy(GorillaRoot)
+		GorillaHum:MoveTo(enemyModel.PrimaryPart.Position)
+		GorillaHum.MoveToFinished:Wait()
+		enemyModel, dist = locateEnemy(GorillaRoot)
+		if dist < 5 then
+			GorillaHum.WalkSpeed = 20
+			return "Attacking"
+		end
+	end
+	
+	if locateEnemy(GorillaRoot) then 
+		return "Charging"
+	else
+		return "Idle"
+	end
 end
 
  control.gorillaStates = {
@@ -277,9 +313,21 @@ end
 	{--Attack | Attack State, when within Radius use an attack.
 		Name = "Attacking",
 
-		NextState = "Attacking", --EnemyCheck if near then charge else idle.
+		NextState = "Idle", --EnemyCheck if near then charge else idle.
 
 		StateAction = attackState_Action,
+
+		Completed = function()
+			warn("Attack Complete")
+		end,
+	},
+	
+	{--Rush | Managing rush behavior in charge proving complex, test attempt
+		Name = "Rush",
+
+		NextState = "Attacking", --EnemyCheck if near then charge else idle.
+
+		StateAction = rushState_Action,
 
 		Completed = function()
 			warn("Attack Complete")
@@ -344,7 +392,7 @@ end
 
 function control:Begin()
 		self.stateThread = task.spawn(function()
-			while task.wait(.1) do 
+			while task.wait(.01) do 
 				if not self.stateComplete then return end 
 				self.stateComplete = false
 			--	warn("--PERFORMING STATE ACTION:")
@@ -355,7 +403,8 @@ function control:Begin()
 			--		warn("--STATE ACTION ERROR, DEFAULTING--")
 					nextState = self.currentState
 				end
-				
+			
+				warn("TRANSITIONED FROM:", self.currentState, "to", nextState)
 				self.currentState = nextState
 				self.stateComplete = true
 			end	
@@ -374,6 +423,5 @@ local gorillaFSM = control.create_StateMachine(gorilla)
 gorillaFSM:defineStates(control.gorillaStates)
 
 gorillaFSM:Begin()
---Start
 
 return control
